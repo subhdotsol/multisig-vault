6a45f021d5425f8ad11805428077aa26b90eb59f
subh <subhajitchaudhury05@gmail.com>
2026-02-26 08:22:43 +0530

    feat : added deposit instruction

--- programs/multisig-vault/src/instructions/deposit_vault.rs
+++ programs/multisig-vault/src/instructions/deposit_vault.rs
@@ -7,3 +7,38 @@
 //    - Depositor (Signer)
 //    - Vault PDA
 //    - System Program
+
+use crate::{events::DepositEvent, transfer_sol, Vault};
+use anchor_lang::prelude::*;
+
+#[derive(Accounts)]
+pub struct DepositVault<'info> {
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+    #[account(mut)]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> DepositVault<'info> {
+    pub fn deposit(&self, amount: u64) -> Result<()> {
+        require!(
+            amount > 0,
+            ErrorCode::from(anchor_lang::error::ErrorCode::ConstraintRaw)
+        );
+
+        transfer_sol(
+            &self.depositor.to_account_info(),
+            &self.vault.to_account_info(),
+            amount,
+            &self.system_program.to_account_info(),
+        )?;
+
+        emit!(DepositEvent {
+            vault: self.vault.key(),
+            depositor: self.depositor.key(),
+            amount,
+        });
+        Ok(())
+    }
+}

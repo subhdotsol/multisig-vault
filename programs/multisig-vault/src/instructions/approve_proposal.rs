9a549cd562bf78d1e7eea9dbb4d228e03daceba1
subh <subhajitchaudhury05@gmail.com>
2026-02-26 08:52:47 +0530

    feat : create approval proposal instructions

--- programs/multisig-vault/src/instructions/approve_proposal.rs
+++ programs/multisig-vault/src/instructions/approve_proposal.rs
@@ -9,3 +9,45 @@
 //    - Approver (Signer)
 //    - Vault PDA
 //    - Proposal PDA
+
+use crate::{
+    errors::VaultError,
+    events::ProposalApproved,
+    utils::helper::{has_already_approved, is_owner},
+    Proposal, Vault,
+};
+use anchor_lang::prelude::*;
+
+#[derive(Accounts)]
+pub struct ApproveProposal<'info> {
+    #[account(mut)]
+    pub approver: Signer<'info>,
+
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub proposal: Account<'info, Proposal>,
+}
+
+impl<'info> ApproveProposal<'info> {
+    pub fn approve(&mut self) -> Result<()> {
+        require!(!self.proposal.executed, VaultError::ProposalAlreadyExecuted);
+        require!(
+            !is_owner(&self.vault, &self.approver.key()),
+            VaultError::NotAnOwner
+        );
+        require!(
+            has_already_approved(&self.proposal, &self.approver.key()),
+            VaultError::AlreadyApproved
+        );
+
+        self.proposal.approvals.push(self.approver.key());
+
+        emit!(ProposalApproved {
+            vault: self.vault.key(),
+            proposal: self.proposal.key(),
+            approver: self.approver.key(),
+            total_approvals: self.proposal.approvals.len() as u8,
+        });
+        Ok(())
+    }
+}

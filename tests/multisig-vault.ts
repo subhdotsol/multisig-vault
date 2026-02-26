24f951224cc62d76b4fa2b4cb2b6113c64c7a98c
subh <subhajitchaudhury05@gmail.com>
2026-02-26 09:22:09 +0530

    feat : Planned all the accounts

--- tests/multisig-vault.ts
+++ tests/multisig-vault.ts
@@ -1,14 +1,55 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MultisigVault } from "../target/types/multisig_vault";
+import { LiteSVM } from "litesvm";
+import { PublicKey, Keypair, SystemProgram } from "@solana/web3.js";
+import { assert } from "chai";
+import * as path from "path";
 
 describe("multisig-vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.multisigVault as Program<MultisigVault>;
+  // Configure the client to use the local cluster
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+
+    const program = anchor.workspace.vaultTurbin as Program<VaultTurbin>;
+    const programId = program.programId;
+
+    let svm: LiteSVM;
+    let payer: Keypair;
+
+    // Planning all the needed accounts
+    let authority : Keypair; 
+    let owner1 : Keypair ; 
+    let owner2 : Keypair ; 
+    let owner3 : Keypair ; 
+
+    let vaultPda : PublicKey ; 
+    let proposalPda : PublicKey ; 
+    
+    const threshold = 2 ; 
+    const depositAmount = new anchor.BN(1_000_000_000); // 1 SOL 
+    const proposalAmount = new anchor.BN(500_000_000); // 0.5 SOL 
+
+
+    before(async () => {
+        svm = new LiteSVM();
+
+        const programParams = {
+            path: path.resolve(__dirname, "../target/deploy/vault_turbin.so"),
+            programId: programId
+        };
+        svm.addProgramFromFile(programParams.programId, programParams.path);
+
+        payer = Keypair.generate();
+        svm.airdrop(payer.publicKey, 10_000_000_000n); // 10 SOL
+
+        vaultKeypair = Keypair.generate();
+    });
 
   it("Is initialized!", async () => {
+
+
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
